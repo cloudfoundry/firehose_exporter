74e3882e0ce7e545a08e4cf0e268d0146b27283c
Ferran Rodenas <frodenas@gmail.com>
2017-10-27 17:47:21 +0200

    Updated vendored libraries

--- vendor/github.com/cloudfoundry-incubator/uaago/client.go
+++ vendor/github.com/cloudfoundry-incubator/uaago/client.go
@@ -4,10 +4,8 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strconv"
 	"strings"
 )
 
@@ -31,11 +29,6 @@ func NewClient(uaaUrl string) (*Client, error) {
 }
 
 func (c *Client) GetAuthToken(username, password string, insecureSkipVerify bool) (string, error) {
-	token, _, err := c.GetAuthTokenWithExpiresIn(username, password, insecureSkipVerify)
-	return token, err
-}
-
-func (c *Client) GetAuthTokenWithExpiresIn(username, password string, insecureSkipVerify bool) (string, int, error) {
 	data := url.Values{
 		"client_id":  {username},
 		"grant_type": {"client_credentials"},
@@ -43,68 +36,27 @@ func (c *Client) GetAuthTokenWithExpiresIn(username, password string, insecureSk
 
 	request, err := http.NewRequest("POST", fmt.Sprintf("%s/oauth/token", c.uaaUrl), strings.NewReader(data.Encode()))
 	if err != nil {
-		return "", -1, err
+		return "", err
 	}
 	request.SetBasicAuth(username, password)
 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	resp, err := c.httpClient(insecureSkipVerify).Do(request)
+	config := &tls.Config{InsecureSkipVerify: insecureSkipVerify}
+	tr := &http.Transport{TLSClientConfig: config}
+	httpClient := &http.Client{Transport: tr}
+
+	resp, err := httpClient.Do(request)
 	if err != nil {
-		return "", -1, err
+		return "", err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return "", -1, fmt.Errorf("Received a status code %v", resp.Status)
+		return "", fmt.Errorf("Received a status code %v", resp.Status)
 	}
 
 	jsonData := make(map[string]interface{})
 	decoder := json.NewDecoder(resp.Body)
 	err = decoder.Decode(&jsonData)
 
-	expiresIn := 0
-	if value, ok := jsonData["expires_in"]; ok {
-		asFloat, err := strconv.ParseFloat(fmt.Sprintf("%f", value), 64)
-		if err != nil {
-			return "", -1, err
-		}
-		expiresIn = int(asFloat)
-	}
-
-	return fmt.Sprintf("%s %s", jsonData["token_type"], jsonData["access_token"]), expiresIn, err
-}
-
-func (c *Client) TokenIsAuthorized(username, password, token, client_id string, insecureSkipVerify bool) (bool, error) {
-
-	request, err := http.NewRequest("POST", fmt.Sprintf("%s/check_token", c.uaaUrl.String()), strings.NewReader("token="+token))
-	if err != nil {
-		return false, err
-	}
-	request.SetBasicAuth(username, password)
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	resp, err := c.httpClient(insecureSkipVerify).Do(request)
-	if err != nil {
-		return false, err
-	}
-	if resp.StatusCode != http.StatusOK {
-		return false, fmt.Errorf("Received a status code %v", resp.Status)
-	}
-	defer resp.Body.Close()
-
-	responseBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return false, err
-	}
-
-	if strings.Contains(string(responseBody), client_id) {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-func (c *Client) httpClient(insecureSkipVerify bool) *http.Client {
-	config := &tls.Config{InsecureSkipVerify: insecureSkipVerify}
-	tr := &http.Transport{TLSClientConfig: config, Proxy: http.ProxyFromEnvironment}
-	return &http.Client{Transport: tr}
+	return fmt.Sprintf("%s %s", jsonData["token_type"], jsonData["access_token"]), err
 }
